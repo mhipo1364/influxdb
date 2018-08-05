9f6aca41a6b32632d7e65e89704392537356db7e
Moe Far <m.torkianfar@irantalent.com>
2018-08-05 19:41:28 +0430

    add project config

---
+++ src/config/ifx_db.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'host' => env('_IFX_HOST', 'localhost'),
+    'database' => env('_IFX_DATABASE', ''),
+    'user' => env('_IFX_USER', ''),
+    'pass' => env('_IFX_PASS', ''),
+    'port' => env('_IFX_PORT', 8086),
+    'timeout' => env('_IFX_TIMEOUT', 0),
+    'connection_timeout' => env('_IFX_CONNECTION_TIMEOUT', 0),
+    'ssl' => env('_IFX_SSL', false),
+    'verify_ssl' => env('_IFX_VERIFY_SSL', false),
+];
